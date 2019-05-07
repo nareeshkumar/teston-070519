57de008be57146eb18c7a0aae3ac2ebac049ba1d
Mandadi <Naresh.K.Mandadi@centurylink.com>
2019-05-07 09:51:54 +0530

    water

--- testfile.py
+++ testfile.py
@@ -3,4 +3,7 @@ if hungry=='yes':
     print('eas samosa')
     print('eat pizza')
 else:
+    thristy=input('enter your feedback')
+    if thristy=='yes':
+        print('drink water')
     print('do you homeword')
